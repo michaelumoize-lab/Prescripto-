da79ec24f78cf4f52cff2e8554d6464bf1a3e1ac
Michael Umoize <michaelumoize@gmail.com>
2026-02-11 14:33:32 +0200

    appointment order fixed

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -63,7 +63,7 @@ const AdminContextProvider = (props) => {
         try {
             const { data } = await axios.get(backendUrl + '/api/admin/appointments', { headers: { aToken } });
             if (data.success) {
-                setAppointments(data.appointments)
+                setAppointments(data.appointments.reverse())
             } else {
                 toast.error(data.message)
             }
